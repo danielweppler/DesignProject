ab7b29d2688f8e9a26fe3f7e2e7a2ff756e21787
Tekkrez <60301808+Tekkrez@users.noreply.github.com>
2020-03-03 14:23:33 -0500

    Add files via upload

--- PID_test.ino
+++ PID_test.ino
@@ -5,16 +5,17 @@
 
 #include <PID_v1.h>
 double leftSetpoint, leftInput, leftOutput;
-double Kp = 2, Ki = 5, Kd = 1;
+double Kp = 1.01, Ki = 0.02, Kd = 0.115;
 double rightSetpoint, rightInput, rightOutput;
 
 const int rightMotorPin = 8;
 const int leftMotorPin = 9;
 
+int lastPrint =0, printInterval = 100;
 
 
 const int countsPerRev = 627.2;
-const int revs = 3;
+const int revs = 10;
 
 //objects
 Servo rightMotor;
@@ -34,6 +35,13 @@ const int rightOffset = 0;
 
 unsigned int rightMotorSpeed;
 unsigned int leftMotorSpeed;
+
+//mode functionality
+bool buttonState;
+bool buttonPreviousState = false;
+int mode = 0;
+
+const int modeButton = 6;
 
 
 void setup() {
@@ -56,9 +64,34 @@ void setup() {
   encoder_RightMotor.init(1.0 / 3.0 * MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
   encoder_RightMotor.setReversed(true);  // adjust for positive count when moving forward
 
+  leftPID.SetMode(AUTOMATIC);
+  rightPID.SetMode(AUTOMATIC);
+
+    //mode button setup
+  pinMode(modeButton, INPUT_PULLUP);
+  
 }
 
 void loop() {
+
+  // mode functionality
+  buttonState = !digitalRead(modeButton);
+
+  if (buttonState && buttonState != buttonPreviousState)
+  {
+    buttonPreviousState = buttonState;
+    mode++;
+    mode = mode & 3;
+  }
+  else if (!buttonState && buttonState != buttonPreviousState)
+  {
+    buttonPreviousState = buttonState;
+  }
+
+
+if(mode ==1){
+  
+  
   // put your main code here, to run repeatedly:
 
   leftInput = encoder_LeftMotor.getRawPosition();
@@ -78,5 +111,22 @@ void loop() {
 
   leftMotor.writeMicroseconds(leftMotorSpeed);
   rightMotor.writeMicroseconds(rightMotorSpeed);
+  
+
+//  Serial.print("SetPoint:");
+  Serial.print(leftSetpoint);
+  Serial.print(" ");
+//  Serial.print("Input:");
+  Serial.print(rightInput);
+  Serial.print(" ");
+//  Serial.print("leftMotorSpeed");
+  Serial.print(rightMotorSpeed);
+  Serial.print(" ");
+
+Serial.println();
+
+
+}
+
 
 }
